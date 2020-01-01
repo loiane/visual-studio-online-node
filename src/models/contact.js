9837dd49cc0399ba0bbba4981943f103f4431cc9
Loiane <loianeg@gmail.com>
2020-01-01 14:00:02 -0500

    first commit

--- src/models/contact.js
+++ src/models/contact.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-    name: {type: String, required: true, max: 200}
+  name: { type: String, required: true, max: 200 }
 });
 
 module.exports = mongoose.model('Contact', ContactSchema);
--- src/services/contact.js
+++ src/services/contact.js
@@ -2,57 +2,57 @@ const Contact = require('../models/contact');
 require('./mongo').connect();
 
 async function getAll(req, res) {
-    try {
-        var records = await Contact.find().exec();
-        res.send(records);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    var records = await Contact.find().exec();
+    res.send(records);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function getById(req, res) {
-    try {
-        var record = await Contact.findById(req.params.id).exec();
-        res.send(record);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    var record = await Contact.findById(req.params.id).exec();
+    res.send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function post(req, res) {
-    try {
-        var record = new Contact(req.body);
-        var record = await record.save();
-        res.status(200).json(record);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    var record = new Contact(req.body);
+    var record = await record.save();
+    res.status(200).json(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function put(req, res) {
-    try {
-        var record = await Contact.findById(req.params.id).exec();
-        record.set(req.body);
-        var record = await record.save();
-        res.send(record);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    var record = await Contact.findById(req.params.id).exec();
+    record.set(req.body);
+    var record = await record.save();
+    res.send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 async function remove(req, res) {
-    try {
-        var record = await Contact.deleteOne({ _id: req.params.id }).exec();
-        res.send(record);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+  try {
+    var record = await Contact.deleteOne({ _id: req.params.id }).exec();
+    res.send(record);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 module.exports = {
-    getAll,
-    getById,
-    post,
-    put,
-    remove
+  getAll,
+  getById,
+  post,
+  put,
+  remove
 };
