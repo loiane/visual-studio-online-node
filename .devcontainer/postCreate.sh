1e7d0adb7fb319eddec2d2c2f8273da28650f00c
Loiane <loianeg@gmail.com>
2020-01-01 14:10:46 -0500

    added dev container files

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo mkdir -p /data/db /data/configdb
+sudo chown -R mongodb:mongodb /data/db /data/configdb
+sudo mongod > ~/mongo.log &
+mongoimport --db tutorials --collection contacts --file ~/workspace/contacts.extjson --drop
+cd ~/workspace
+npm i
